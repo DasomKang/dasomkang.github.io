40d2fc1ac42ce6268f0b9e956de3d23c285285d8
Dasom <dskang.8764@gmail.com>
2018-11-03 15:46:14 +0100

    Added R markdown

---
+++ R_markdown.R
@@ -0,0 +1,30 @@
+# jekyll 블로그 디렉토리 설정
+base <- "/Users/dasomkang/dasomkang.github.io/"
+
+# Rmd파일이 저장될 디렉토리 지정
+rmds <- "_Rmd"
+setwd(base)
+
+# 파일 이름 지정
+filename <- "2016-01-18-R-graphics-cookbook-chapter-3.Rmd"
+
+# 폴더 경로들
+figs.path <- "assets/article_images/"
+posts.path <- "_posts/R/"
+
+# START!!!
+require(knitr)
+render_jekyll(highlight = "pygments")
+opts_knit$set(base.url="/")
+
+file <- paste0(rmds, "/", filename)
+
+# 파일 경로 설정
+fig.path <- paste0(figs.path, sub(".Rmd$", "", basename(file)), "/")
+opts_chunk$set(fig.path = fig.path)
+
+# suppress messages
+opts_chunk$set(cache = F, warning = F, message = F, cache.path = "_cache/", tidy = F)
+
+out.file <- basename(knit(file))
+file.rename(out.file, paste0(posts.path, out.file))
